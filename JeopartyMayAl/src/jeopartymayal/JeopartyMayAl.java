dc7f723834c2e963eae4ed76008a8a94e7e4f6e6
silveiraalexand <silveiraalexand@10.96.25.157>
2016-09-23 20:30:16 -0300

    Primeiro Commit!

---
+++ JeopartyMayAl/src/jeopartymayal/JeopartyMayAl.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeopartymayal;
+
+/**
+ *
+ * @author silveiraalexand
+ */
+public class JeopartyMayAl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+        //Threads ------------------------------------------
+        
+        //Single Thread
+        
+        Thread th = new Thread();
+        
+        
+        
+        //MultiThread
+        
+        Thread th1 = new Thread();
+        Thread th2 = new Thread();
+        Thread th3 = new Thread();
+        Thread th4 = new Thread();
+        
+        
+        
+    }
+    
+}
